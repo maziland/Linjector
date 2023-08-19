012a734241e865cf50f2e6b032a17aabfdc0602e
maziland <alon951m@gmail.com>
2023-08-19 20:33:42 +0300

    main.rs now contains only highlevel logic

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
-use std::process::exit;
-
 use log::{self};
-use nix::{
-    sys::{ptrace, signal::Signal},
-    unistd::Pid,
-};
-use pretty_env_logger;
-use sysinfo::{PidExt, ProcessExt, System, SystemExt};
 
+mod debugee;
 mod utils;
-fn main() {
+
+use debugee::{Debugee, DebugeeResult};
+use nix::libc::{MAP_ANONYMOUS, MAP_PRIVATE, PROT_READ, PROT_WRITE};
+use syscalls::Sysno;
+fn main() -> DebugeeResult<()> {
     // Read config file
     let injector_config = utils::read_config("config");
 
@@ -24,41 +21,20 @@ fn main() {
         injector_config.log_level_filter
     );
 
-    // Find process instances
-    let system = System::new_all();
-    let process_instances = system
-        .processes_by_exact_name(&injector_config.process_name)
-        .take(2)
-        .collect::<Vec<_>>();
+    let mut debugee = Debugee::new(injector_config.process_name);
+    debugee.attach();
+    let mem_address = debugee.syscall(
+        Sysno::mmap,
+        0,
+        10,
+        (PROT_READ | PROT_WRITE) as u64,
+        (MAP_PRIVATE | MAP_ANONYMOUS) as u64,
+        u64::MAX,
+        0,
+    )?;
 
-    // Verify there's only one
-    match process_instances.len() {
-        0  => {log::error!("Process not found!"); exit(-1)},
-        2 => {log::error!("Found more than 1 process for the given name!"); exit(-1)},
-        _ => {} // Gets here if there's 1 - should continue executing
-    }
+    // let mem_address = debugee.syscall(Sysno::exit, 42, 0, 0, 0, 0, 0)?;
+    debugee.detach();
 
-    let process = process_instances[0];
-    log::info!(
-        "Got name: '{}' with pid: {}",
-        process_instances[0].name(),
-        process_instances[0].pid()
-    );
-
-    // Ptrace attach
-    let pid = Pid::from_raw(process.pid().as_u32() as i32);
-    ptrace::attach(pid).unwrap_or_else(|error| {
-        log::error!("Failed attaching to process, error: {}", error);
-        exit(-1)
-    });
-    log::info!("Successfuly attached pid {}", pid);
-
-    /*
-        PTRACE LOGIC GOES HERE
-    */
-
-    // Ptrace detach
-    ptrace::detach(pid, Signal::SIGCONT)
-        .unwrap_or_else(|error| log::info!("Failed detaching from process, error: {}", error));
-    log::info!("Successfuly detached pid {}", pid);
+    Ok(())
 }
