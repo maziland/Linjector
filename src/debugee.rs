20c57908c9b8555ac984a3d80940aaeb7e02e51f
Maziland <alon951m@gmail.com>
2023-11-19 23:26:49 +0200

    Moved to pete module, Syscalls are now working

--- src/debugee.rs
+++ src/debugee.rs
@@ -1,5 +1,6 @@
-use std::{ffi::c_void, process::exit};
+use std::{ffi::c_void, mem, process::exit};
 
+use crate::utils::get_pid_from_process_name;
 use log::{self};
 use nix::{
     libc::user_regs_struct,
@@ -9,13 +10,11 @@ use nix::{
         wait::{waitpid, WaitStatus},
     },
 };
-use sysinfo::{PidExt, ProcessExt, System, SystemExt};
+use pete::{Ptracer, Tracee};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum DebugeeErr {
-    #[error("Process not found `{0}`")]
-    ProcessNotFound(String),
     #[error("Nix Error `{0}`")]
     NixError(#[from] nix::errno::Errno),
     #[error("Unexpected Wait Status `{0:#?}`")]
@@ -26,36 +25,59 @@ pub enum DebugeeErr {
     MmapBadAddress(u64),
     #[error("Munmap Error `{0:#?}`")]
     MunmapFailed(u64),
+    #[error("Pete Error: `{0:#?}`")]
+    PeteError(#[from] pete::Error),
 }
 
 pub type DebugeeResult<T> = Result<T, DebugeeErr>;
 pub struct Debugee {
     pub pid: nix::unistd::Pid,
     pub process_name: String,
+    pub tracee: Option<Tracee>,
     is_attached: bool,
 }
 
 impl Debugee {
     pub fn new(process_name: String) -> Self {
-        let dbg: Debugee = Debugee {
-            pid: get_pid_from_process_name(process_name.as_str()),
+        let pid = get_pid_from_process_name(process_name.as_str());
+        let debugee: Debugee = Debugee {
+            pid,
             process_name,
+            tracee: None,
             is_attached: false,
         };
-        dbg
+        debugee
+    }
+
+    fn get_tracee(&self) -> Tracee {
+        if let Some(tracee) = self.tracee {
+            tracee.to_owned()
+        } else {
+            log::error!("Problem obtaining tracee");
+            exit(-1);
+        }
     }
 
     pub fn attach(&mut self) {
-        ptrace::attach(self.pid).unwrap_or_else(|error| {
-            log::error!("Failed attaching to process, error: {}", error);
-            exit(-1);
-        });
-        self.is_attached = true;
-        log::info!("Successfuly attached pid {}", self.pid);
+        let mut ptracer = Ptracer::new();
+        let _ = ptracer.attach(self.pid);
+
+        match ptracer.wait() {
+            Ok(tracee) => {
+                log::info!("Successfuly attached pid {}", self.pid);
+                self.is_attached = true;
+                self.tracee = tracee;
+                tracee
+            }
+            _ => {
+                log::error!("Failed attaching to process");
+                exit(-1)
+            }
+        };
     }
 
     pub fn detach(&mut self) {
-        if (self.is_attached) {
+        if self.is_attached {
             // Detach with ptrace
             match ptrace::detach(self.pid, Signal::SIGCONT) {
                 Ok(()) => {
@@ -89,24 +111,26 @@ impl Debugee {
     }
 
     // Wrapping ptrace write
-    fn write(&self, addr: *mut c_void, data: *mut c_void) -> DebugeeResult<()> {
-        log::trace!("writing to address: {:#?}", addr);
-        unsafe { ptrace::write(self.pid, addr, data).map_err(DebugeeErr::NixError) }
+    pub fn write(&mut self, addr: u64, data: &[u8]) -> DebugeeResult<usize> {
+        self.get_tracee()
+            .write_memory(addr, data)
+            .map_err(DebugeeErr::PeteError)
     }
 
     // Wrapping ptrace read
-    fn read(&self, addr: *mut c_void) -> DebugeeResult<i64> {
-        log::trace!("reading from address: {:#?}", addr);
-        ptrace::read(self.pid, addr).map_err(DebugeeErr::NixError)
+    pub fn read(&self, addr: u64, len: usize) -> DebugeeResult<Vec<u8>> {
+        self.get_tracee()
+            .read_memory(addr, len)
+            .map_err(DebugeeErr::PeteError)
     }
 
     pub fn syscall(
-        &self,
+        &mut self,
         syscall: syscalls::Sysno,
         rdi: u64,
         rsi: u64,
         rdx: u64,
-        rcx: u64,
+        r10: u64,
         r8: u64,
         r9: u64,
     ) -> DebugeeResult<user_regs_struct> {
@@ -114,16 +138,18 @@ impl Debugee {
             log::error!("There is no debugee attached");
             exit(-1);
         }
-
-        let backup_registers = ptrace::getregs(self.pid)?;
+        let syscall_name = syscall.name();
+        let syscall_number = syscall.id();
+        let backup_registers = self.get_tracee().registers()?;
 
         // In order to syscall, we need to set RIP to a SYSCALL gadget
-        let original_rip = backup_registers.rip as *mut c_void;
-        let original_rip_opcodes = self.read(original_rip)? as *mut c_void;
+        let original_rip = backup_registers.rip;
+        let original_rip_opcodes = self.read(original_rip, mem::size_of::<u64>())?;
 
         // Write our syscall opcode to RIP
-        let syscall_opcode = u16::from_le_bytes([0x0F, 0x05]) as *mut c_void;
-        self.write(original_rip, syscall_opcode)?;
+        // let syscall_opcode = u16::from_le_bytes([0x0F, 0x05]);
+        let syscall_opcode: &[u8] = &[0x0F, 0x05];
+        self.write(original_rip, &syscall_opcode)?;
 
         // Provide registers for the syscall
         let mut new_registers = backup_registers;
@@ -131,7 +157,7 @@ impl Debugee {
         new_registers.rdi = rdi;
         new_registers.rsi = rsi;
         new_registers.rdx = rdx;
-        new_registers.rcx = rcx;
+        new_registers.r10 = r10;
         new_registers.r8 = r8;
         new_registers.r9 = r9;
 
@@ -140,13 +166,16 @@ impl Debugee {
         });
 
         log::info!("Successfuly changed syscall opcode and registers");
+        log::trace!(
+            "Executing `{syscall_name}`({syscall_number}) with: {rdi}, {rsi}, {rdx}, {r10}, {r8}, {r9}"
+        );
         self.single_step()?;
-
         let result = ptrace::getregs(self.pid)?;
 
         // Restore original instructions, and original registers to continue normal program control flow
-        self.write(original_rip, original_rip_opcodes)?;
-        ptrace::setregs(self.pid, backup_registers)?;
+        self.write(original_rip, &original_rip_opcodes)?;
+
+        self.get_tracee().set_registers(backup_registers)?;
         log::info!("Restored opcodes and registers");
 
         log::info!("Syscall return value: {:#?}", result.rax as *mut c_void);
@@ -162,30 +191,4 @@ impl Drop for Debugee {
             self.detach();
         }
     }
-}
-
-fn get_pid_from_process_name(process_name: &str) -> nix::unistd::Pid {
-    let system = System::new_all();
-    let process_instances = system
-        .processes_by_exact_name(process_name)
-        .take(2)
-        .collect::<Vec<_>>();
-
-    // Verify there's only one
-    match process_instances.len() {
-        0 => {
-            log::error!("Process not found!");
-            exit(-1)
-        }
-        2 => {
-            log::error!("Found more than 1 process for the given name!");
-            exit(-1)
-        }
-        _ => {} // Gets here if there's 1 - should continue executing
-    }
-
-    let process = process_instances[0];
-    log::info!("Found '{}' with pid: {}", process.name(), process.pid());
-    let pid_u32 = PidExt::as_u32(process.pid());
-    nix::unistd::Pid::from_raw(pid_u32 as i32)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use log::{self};
+use syscalls::Sysno;
 
 mod debugee;
 mod utils;
 
-use debugee::{Debugee, DebugeeResult};
+use crate::debugee::Debugee;
+use debugee::DebugeeResult;
 use nix::libc::{MAP_ANONYMOUS, MAP_PRIVATE, PROT_READ, PROT_WRITE};
-use syscalls::Sysno;
 fn main() -> DebugeeResult<()> {
     // Read config file
     let injector_config = utils::read_config("config");
@@ -16,25 +17,29 @@ fn main() -> DebugeeResult<()> {
         .init();
 
     log::trace!(
-        "built config with: process_name: '{}', log_level_filter: {}",
-        injector_config.process_name,
-        injector_config.log_level_filter
+        "built config with: process_name: '{}'",
+        injector_config.process_name
     );
 
     let mut debugee = Debugee::new(injector_config.process_name);
     debugee.attach();
-    let mem_address = debugee.syscall(
-        Sysno::mmap,
-        0,
-        10,
-        (PROT_READ | PROT_WRITE) as u64,
-        (MAP_PRIVATE | MAP_ANONYMOUS) as u64,
-        u64::MAX,
-        0,
-    )?;
+    let _mem_address = debugee
+        .syscall(
+            Sysno::mmap,
+            0,                                    // start
+            11,                                   // len
+            (PROT_READ | PROT_WRITE) as u64,      // prot
+            (MAP_PRIVATE | MAP_ANONYMOUS) as u64, // flags
+            0,                                    // fd
+            0,                                    // offset
+        )
+        .unwrap()
+        .rax;
 
-    // let mem_address = debugee.syscall(Sysno::exit, 42, 0, 0, 0, 0, 0)?;
-    debugee.detach();
-
+    debugee.write(_mem_address, &[1, 2, 3, 4, 5])?;
+    let mem = debugee.read(_mem_address, 11)?;
+    for a in mem {
+        log::trace!("{a}");
+    }
     Ok(())
 }
